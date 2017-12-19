c642a6693690185e16e08bae4d94edbda7b8918b
Wilder Lizama <wilderlizama@gmail.com>
2017-12-19 17:45:54 -0500

    Variables en C

---
+++ Variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define numeroConst 30
+
+void main(){
+	
+	int numeroVariable = 120;
+	int resultado;
+
+	resultado = numeroVariable + numeroConst;
+
+	printf("Resultado de la suma es %d \n", resultado);
+}
